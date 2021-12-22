2414cb696b5f4366a8286d4f408fb69982937560
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2021-12-22 11:18:33 +0800

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Program:
+# This program will auto install mavproxy
+# History:
+# 2021/12/22  Sirius  First release
+PATH=/home/pi/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games:/home/pi/.local/bin
+export PATH
+read ip
+read ovpn_path
+sudo scp sirius2beta@${ip}:${ovpn_path} .
+sudo mv [name].ovpn client.conf
