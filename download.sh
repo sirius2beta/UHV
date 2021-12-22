39a2fe7289015d409ae22ebcb3103c5804d88f34
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2021-12-22 10:56:32 +0800

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Program:
+# This program will auto install mavproxy
+# History:
+# 2021/12/22  Sirius  First release
+PATH=/home/pi/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games:/home/pi/.local/bin
+export PATH
+echo "start downloading mavproxy"
+yes Y |sudo apt-get install python3-dev python3-opencv python3-wxgtk4.0 python3-pip python3-matplotlib python3-lxml python3-pygame
+pip3 install PyYAML mavproxy --user
+echo "export PATH=$PATH:$HOME/.local/bin" >> ~/.bashrc
+source ~/.bashrc
