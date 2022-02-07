690ab0d282d1d38fdca08bceafc5f837f727287f
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2022-02-07 11:44:42 +0800

    Delete qcrmcall.sh

--- qcrmcall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/expect
-#Program:
-# This program will auto install mavproxy
-# History:
-# 2021/12/22  Sirius  First release
-spawn minicom -D /dev/ttyUSB2
-expect "Press CTRL-A Z for help"
-send "AT\$QCRMCALL=1,1\r"
