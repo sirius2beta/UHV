6ea1308b2255fcf962e3166ba35d5fa94e37051e
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2022-02-07 11:44:25 +0800

    Delete connect.sh

--- connect.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/expect
-#Program:
-# This program will auto install mavproxy
-# History:
-# 2021/12/22  Sirius  First release
-set timeout 3
-spawn minicom -S connect.txt -D /dev/ttyUSB2
-expect 1,1
-expect 1,1
