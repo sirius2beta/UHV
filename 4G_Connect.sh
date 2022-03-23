ec980128757533945b32d31d216c21fee4170806
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2022-03-24 02:25:15 +0800

    Update 4G_Connect.sh

--- 4G_Connect.sh
+++ 4G_Connect.sh
@@ -5,6 +5,7 @@
 # 2022/1/10  Sirius  First release
 PATH=/home/pi/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games:/home/pi/.local/bin
 export PATH
+sleep 5s
 sudo -s rmmod qmi_wwan
 sudo -s cd /home/pi/USVPI/SIM7600_NDIS
 sudo -s insmod /home/pi/USVPI/SIM7600_NDIS/simcom_wwan.ko
