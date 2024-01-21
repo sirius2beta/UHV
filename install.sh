9b09878d3b52cdcc794d0c9011d309d67242c3cb
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2024-01-21 21:14:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,24 +15,6 @@ pip3 install PyYAML mavproxy --user
 echo "export PATH=$PATH:$HOME/.local/bin" >> ~/.bashrc
 source ~/.bashrc
 
-#echo "start downloading openvpn..."
-#yes Y |sudo apt-get install openvpn
-#yes Y |sudo apt install mosquitto mosquitto-clients
-#yes Y |sudo apt-get install libmosquitto-dev libmosquittopp-dev libssl-dev
-
-echo "start installing gstreamer"
-yes Y |sudo apt-get install libx264-dev libjpeg-dev
-yes Y |sudo apt-get install libgstreamer1.0-dev \
-		     libgstreamer-plugins-base1.0-dev \
-		     libgstreamer-plugins-bad1.0-dev \
-		     gstreamer1.0-plugins-ugly \
-     gstreamer1.0-tools
-yes Y |sudo apt-get install gstreamer1.0-gl gstreamer1.0-gtk3
-
-echo "start installing paho-mqtt"
-yes Y | pip install paho-mqtt
-yes Y | sudo pip install paho-mqtt
-
 echo "start installing minicom"
 yes Y |sudo apt-get install minicom
 
@@ -48,16 +30,6 @@ sudo -s make
 yes Y |sudo apt-get install udhcpc
 cd ..
 sudo chmod +x 4G_Connect.sh
-echo "finished installation process"
-
-#echo "Install zerotier"
-#yes Y |sudo apt upgrade
-#curl https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/zerotierone-archive-keyring.gpg >/dev/null
-#RELEASE=$(lsb_release -cs)
-#echo "deb [signed-by=/usr/share/keyrings/zerotierone-archive-keyring.gpg] http://download.zerotier.com/debian/$RELEASE $RELEASE main" | sudo tee /etc/apt/sources.list.d/zerotier.list
-#yes Y |sudo apt update
-#yes Y |sudo apt install -y zerotier-one
-#sudo zerotier-cli join e5cd7a9e1c74ff6e
 
 echo "Install Tailscale"
 yes Y | sudo apt update
@@ -73,11 +45,10 @@ sudo tailscale up --authkey tskey-auth-kp9gYY3CNTRL-SsRgKrmfgNRr6ibkC7QoFRhNyHf1
 yes Y | sudo apt-get update
 yes Y | sudo apt-get install realvnc-vnc-server
 
-echo "start dowloading gstreamer_singlecamera"
+echo "start dowloading GPlayer3"
 cd
 git clone https://github.com/sirius2beta/USVPI.git
 git clone https://github.com/sirius2beta/sensor.git
-git clone https://github.com/sirius2beta/GStreamer_singlecamera.git
 git clone https://github.com/sirius2beta/GPlayer3.git
 cd GPlayer3
 sudo chmod +x install.sh
